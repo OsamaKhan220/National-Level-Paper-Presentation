0b7ef1a498eefeed36545997dea2adae01ae2d9f
OsamaKhan220 <osama.ahmedkhan96@gmail.com>
2019-03-23 00:42:33 +0500

    add project

---
+++ ksc/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ksc.Models;
+
+namespace ksc.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected KSCEntities db = new KSCEntities();
+    }
+}
---
+++ ksc/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ksc.Models.AppModels;
+
+namespace ksc.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginRequest model) {
+            var user = db.Users.Where(e => e.email == model.Email && e.password == model.Password).FirstOrDefault();
+            if (user != null) {
+                return RedirectToAction("Index","Dashboard");
+            }
+            ViewBag.Message = "Invalid Credentials";
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Register() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterRequest model) {
+            var user = db.Users.Where(e => e.email == model.Email).FirstOrDefault();
+            if (user == null)
+            {
+                ViewBag.Message = "User Registered.";
+            }
+            else {
+                ViewBag.Message = "User Already Exist.";
+            }
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
