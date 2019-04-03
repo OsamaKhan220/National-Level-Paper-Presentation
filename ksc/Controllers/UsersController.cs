1a02c3bb01098ed592369e76e35bad62038c723b
Salman Saleem <salman@kumlaudi.com>
2019-04-03 10:30:47 +0500

    Edit and add users

--- ksc/Controllers/UsersController.cs
+++ ksc/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ksc.Models;
 
 namespace ksc.Controllers
 {
@@ -11,13 +12,26 @@ namespace ksc.Controllers
         // GET: Users
         public ActionResult Index()
         {
+            
             var users = db.Users.ToList();
             return View(users);
         }
 
         public ActionResult Edit(int Id) {
+            ViewBag.roles = db.Roles.ToList();
             var user = db.Users.Where(e => e.Id == Id).FirstOrDefault();
             return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User model)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Add() {
+            ViewBag.roles = db.Roles.ToList();
+            return View();
         }
     }
 }
